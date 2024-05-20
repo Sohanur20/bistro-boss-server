4979a82b35dba3567be4fd80b2b0baa1a2b1f199
Sohanur20 <mdsohank168@gmail.com>
2024-05-20 17:51:57 +0600

    cart finded

--- index.js
+++ index.js
@@ -27,6 +27,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const menuCollection = client.db("BistroDB").collection("menu")
+const reviewsCollection = client.db("BistroDB").collection("reviews")
+const cartCollection = client.db("BistroDB").collection("carts")
+
+app.get('/menu' , async(req, res ) =>{
+const result = await menuCollection.find().toArray()
+res.send(result)
+})
+app.get('/reviews' , async(req, res ) =>{
+const result = await reviewsCollection.find().toArray()
+res.send(result)
+})
+
+// cart collection 
+app.get('/carts' , async(req , res ) =>{
+  const result = await cartCollection.find().toArray()
+  res.send(result)
+})
+
+app.post('/carts' , async ( req, res ) =>{
+  const cartItem = req.body ;
+  const result = await cartCollection.insertOne(cartItem)
+  res.send(result)
+})
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
