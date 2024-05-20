434a6fae327be3d0c857470f48727e765527a448
Sohanur20 <mdsohank168@gmail.com>
2024-05-21 01:09:16 +0600

    deleted successfully done on data base

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 5000
@@ -43,7 +43,9 @@ res.send(result)
 
 // cart collection 
 app.get('/carts' , async(req , res ) =>{
-  const result = await cartCollection.find().toArray()
+  const email = req.query.email
+  const query = {email : email};
+  const result = await cartCollection.find(query).toArray()
   res.send(result)
 })
 
@@ -53,6 +55,13 @@ app.post('/carts' , async ( req, res ) =>{
   res.send(result)
 })
 
+
+app.delete('/carts/:id' , async(req, res) =>{
+  const id = req.params.id
+  const query = {_id : new ObjectId (id)}
+  const result = await cartCollection.deleteOne(query);
+  res.send(result)
+})
 
 
 
